f1834ccbb6713d307e21bb31bf1e321512842940
fernaldya <fwirjowerdojo@gmail.com>
2025-08-27 16:31:24 +0700

    Working production level

--- backend/utils/connect_db.py
+++ backend/utils/connect_db.py
@@ -1,9 +1,9 @@
 import os
 from psycopg_pool import ConnectionPool
-import logging
+from urllib.parse import quote_plus
 
 DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_PASS = quote_plus(os.getenv("DB_PASS"))
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT", "5432")
 DB_NAME = os.getenv("DB_NAME", "postgres")
