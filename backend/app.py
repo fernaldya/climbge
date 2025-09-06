d038f4ac11065a62788a1012c9fabbdf472fc785
fernaldya <fwirjowerdojo@gmail.com>
2025-09-06 23:24:24 +0700

    fixed env var

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-env_file = os.environ.get('ENV_FILE', '.env')
+env_file = os.environ.get('ENV_DIR', '.env')
 if os.path.exists(env_file):
     load_dotenv(f'{os.environ.get('ENV_DIR')}')
 
