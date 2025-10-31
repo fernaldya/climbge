cf7ae8e72e7204938ef2db1cbaeacbf41408792c
fernaldya <fwirjowerdojo@gmail.com>
2025-10-31 21:05:53 +0700

    Tiny update

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-f8c404e3'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.ksc5svs7qt"
+    "revision": "0.81e4c53p94o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
