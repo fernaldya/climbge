6f1abe9a04a3d1eb6815fdf638a9bc81c554d3ea
fernaldya <fwirjowerdojo@gmail.com>
2026-05-27 23:58:39 +0700

    Backend for climbing location

--- backend/services/feedback_service.py
+++ backend/services/feedback_service.py
@@ -3,6 +3,7 @@ from utils.http import err
 from utils.connect_db import pool
 
 def submit_feedback(user_id: str, text: str):
+    """Inserts user feedback"""
     if not text:
         return err("invalid_request", "Feedback is required.", 400)
     if len(text) > 4000:
@@ -19,6 +20,32 @@ def submit_feedback(user_id: str, text: str):
                 "INSERT INTO public.user_feedback (user_id, username, feedback) VALUES (%s, %s, %s)",
                 (user_id, row["username"], text),
             )
+        return {"ok": True}, 200
+    except Exception:
+        return err("db_error", "Database error.", 500)
+
+
+def submit_new_climb_location(user_id: str, payload: dict):
+    """Inserts new climbing location to the pending list"""
+    if not payload:
+        return {"error": "No payload"}, 400
+
+    new_location = payload.get('newLocation', {})
+    gym_name = new_location.get('gymName')
+    gym_chain = new_location.get('gymChain')
+    gym_location = new_location.get('gymLocation')
+    country_code = new_location.get('countryCode')
+
+    if not gym_name or not gym_location:
+        return {'error': 'Gym name and location not provided'}, 400
+
+    try:
+        with pool.connection() as conn, conn.transaction(), conn.cursor(row_factory=dict_row) as cur:
+            cur.execute(
+                "INSERT INTO climbing_locations (gym_name, gym_chain, location, country_code, submitted_by) VALUES (%s, %s, %s, %s, %s)",
+                (gym_name, gym_chain, gym_location, country_code, user_id),
+            )
+
         return {"ok": True}, 200
     except Exception:
         return err("db_error", "Database error.", 500)
