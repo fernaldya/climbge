65a56f4fb78d635fba4196dfe5771e9c797c1c03
fawfawfaw <faw.wirjowerdojo@gmail.com>
2026-07-24 13:57:39 +0700

    More attempt at fixing overflow

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,12 +1,10 @@
-import { forwardRef } from "react";
 import { cn } from "./utils";
 
 type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
-export const Input = forwardRef<HTMLInputElement, Props>(function Input({ className, ...props }, ref) {
+export function Input({ className, ...props }: Props) {
   return (
     <input
-      ref={ref}
       className={cn(
         "flex h-10 w-full rounded-xl border bg-background px-3 py-2 text-sm outline-none",
         "border-border focus:ring-2 focus:ring-primary/40",
@@ -15,4 +13,4 @@ export const Input = forwardRef<HTMLInputElement, Props>(function Input({ classN
       {...props}
     />
   );
-});
+}
