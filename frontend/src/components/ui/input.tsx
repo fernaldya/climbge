b06634c0fed0a11174ab61a43eada9c6a544d89b
fawfawfaw <faw.wirjowerdojo@gmail.com>
2026-07-24 12:50:47 +0700

    Moved date and time to be above one another to fix overflow and overlap

--- frontend/src/components/ui/input.tsx
+++ frontend/src/components/ui/input.tsx
@@ -1,8 +1,12 @@
+import { forwardRef } from "react";
 import { cn } from "./utils";
+
 type Props = React.InputHTMLAttributes<HTMLInputElement>;
-export function Input({ className, ...props }: Props) {
+
+export const Input = forwardRef<HTMLInputElement, Props>(function Input({ className, ...props }, ref) {
   return (
     <input
+      ref={ref}
       className={cn(
         "flex h-10 w-full rounded-xl border bg-background px-3 py-2 text-sm outline-none",
         "border-border focus:ring-2 focus:ring-primary/40",
@@ -11,4 +15,4 @@ export function Input({ className, ...props }: Props) {
       {...props}
     />
   );
-}
+});
